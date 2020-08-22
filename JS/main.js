2bd5256eac0148c7d2ecbbc2327016c2e056ebe9
Nikita <crusssader@icloud.com>
2020-08-22 15:35:48 +0300

    добавили canvas

---
+++ JS/main.js
@@ -0,0 +1,5 @@
+var canvas = document.getElementById("canvas"); //получили элемент canvas
+var ctx = canvas.getContext("2d"); //используем элемент canvas в контексте 2D
+var width = canvas.width; // ширина холста
+var height = canvas.height;// высота холста
+
