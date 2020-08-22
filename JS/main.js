4156e04a6715e52db661d9a3cd835e8e35e9c2d9
Nikita <crusssader@icloud.com>
2020-08-22 16:00:57 +0300

    добавлены функции отрисовки

--- JS/main.js
+++ JS/main.js
@@ -3,3 +3,32 @@ var ctx = canvas.getContext("2d"); //используем элемент canvas 
 var width = canvas.width; // ширина холста
 var height = canvas.height;// высота холста
 
+var blockSize = 10; //размер блока 
+var widthInBlocks = width/blockSize; // количество блоков зависит от заданой ширины и высоты
+var heightInBlocks = height/blockSize;
+
+var score = 0; //счёт
+
+var drawBorder = function() {   // функция отрисовки границы
+    ctx.fillStyle = "Gray";
+    ctx.fillRect(0,0,width,blockSize);
+    ctx.fillRect(0,height-blockSize,width,blockSize);
+    ctx.fillRect(0,0,blockSize,height);
+    ctx.fillRect(width-blockSize,0,blockSize,height);
+};
+var drawScore = function() {   // функция отрисовки счёта
+    ctx.font = "20px Courier";
+    ctx.fillStyle = "Black";
+    ctx.textAlign = "left";
+    ctx.textBaseline = "top";
+    ctx.fillText("Счёт: " + score,blockSize,blockSize);
+};
+var gameOver = function() {   // функция отрисовки Конец игры. и остановка интервала
+    clearInterval(intervalId);
+    ctx.font = "60px Courier";
+    ctx.fillStyle = "Black";
+    ctx.textAlign = "center";
+    ctx.textBaseline = "middle";
+    ctx.fillText("Конец игры",width/2,height/2);
+};
+
