09347714b90393d26f650b0de3ad086cce04fc21
Nikita <crusssader@icloud.com>
2020-08-30 12:47:04 +0300

    добавил функцию отрисовки яблока

--- JS/main.js
+++ JS/main.js
@@ -36,3 +36,33 @@ let Block = function(col, row) { //констуктор Block
     this.col = col;
     this.row = row;
 };
+
+
+
+//let drawBorder = function() {   // функция отрисовки границы
+ctx.fillStyle = "Gray";
+ctx.fillRect(0,0,width,blockSize);
+ctx.fillRect(0,height-blockSize,width,blockSize);
+ctx.fillRect(0,0,blockSize,height);
+ctx.fillRect(width-blockSize,0,blockSize,height);
+};
+let drawScore = function() {   // функция отрисовки счёта
+ctx.font = "20px Courier";
+ctx.fillStyle = "Black";
+ctx.textAlign = "left";
+ctx.textBaseline = "top";
+ctx.fillText("Счёт: " + score,blockSize,blockSize);
+};
+let gameOver = function() {   // функция отрисовки Конец игры. и остановка интервала
+clearInterval(intervalId);
+ctx.font = "60px Courier";
+ctx.fillStyle = "Black";
+ctx.textAlign = "center";
+ctx.textBaseline = "middle";
+ctx.fillText("Конец игры",width/2,height/2);
+};
+
+let Block = function(col, row) { //констуктор Block
+this.col = col;
+this.row = row;
+};
